b27f4b84059a027e6c9f7adbc042437006580e74
Team 6036 Drivestation <frc6036@gmail.com>
2020-02-08 16:37:47 -0800

    Add files via upload

---
+++ collectAndRemoveLogsFromRoborio.cmd
@@ -0,0 +1,14 @@
+@echo off
+set datetime=%DATE:/=-%-%time::=-%
+set name=%datetime: =-%
+
+
+if not exist "logs" mkdir logs
+cd logs
+
+mkdir %name:.=-%
+cd %name:.=-%
+
+scp -r admin@roboRIO-6036-FRC.local:/home/admin/logs/ .
+
+ssh admin@roboRIO-6036-FRC.local "rm -rf /home/admin/logs"
